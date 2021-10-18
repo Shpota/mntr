03536ef404883ac49ac3b10908c4364b50024512
Sasha Shpota <5640984+Shpota@users.noreply.github.com>
2021-10-18 21:18:01 +0300

    Add client

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import ServicesTable from "./ServicesTable";
+import AddServiceForm from "./AddServiceForm";
+
+function App() {
+    return (
+        <div className="App">
+            <ServicesTable/>
+            <AddServiceForm/>
+        </div>
+    );
+}
+
+export default App;
---
+++ client/src/ServicesTable.js
@@ -0,0 +1,49 @@
+import axios from "axios";
+import {useEffect, useState} from "react";
+import useWebSocket from "react-use-websocket";
+import "./ServicesTable.css"
+
+function ServicesTable() {
+    const [services, setServices] = useState([]);
+    const {lastJsonMessage} = useWebSocket(`${process.env.REACT_APP_WS_API}/services`, {shouldReconnect: (closeEvent) => true});
+
+    useEffect(() => {
+        axios.get(`${process.env.REACT_APP_REST_API}/services`)
+            .then(resp => setServices(resp.data))
+    }, []);
+
+    useEffect(() => {
+        if (services && lastJsonMessage) {
+            const index = services.findIndex((it) => it.id === lastJsonMessage.id)
+            if (index === -1) {
+                setServices([...services, lastJsonMessage]);
+            } else {
+                const items = [...services];
+                items[index] = lastJsonMessage;
+                setServices(items);
+            }
+        }
+    }, [lastJsonMessage])
+
+    return (
+        <>
+            {services && services.length > 0 && (<table className="services-table">
+                <tbody>
+                <tr>
+                    <th>Name</th>
+                    <th>URL</th>
+                    <th>Status</th>
+                </tr>
+                {services.map((service, i) => <tr key={i}>
+                    <td>{service.name}</td>
+                    <td>{service.url}</td>
+                    <td>{service.status}</td>
+                </tr>)}
+                </tbody>
+            </table>)}
+            {(!services || services.length === 0) && (<div>There are no services to monitor</div>)}
+        </>
+    );
+}
+
+export default ServicesTable;
